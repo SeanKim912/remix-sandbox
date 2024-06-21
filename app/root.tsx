9ed0eefbc3764ce02a3d829692c135c4d78be669
Sean <seankim912@yahoo.com>
2024-06-20 23:44:47 -0500

    route setup from scratch

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,29 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+import { LiveReload, Outlet } from "@remix-run/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Remix Sandbox</title>
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        Whaazzzuuuppppp
+        <Outlet />
+        {/*
+          This component renders child route elements,
+          if there is one (index route). Essential!
+        */}
+        <LiveReload />
+        {/*
+          LiveReload component is useful during development to auto-refresh browser
+          whenever you make changes.
+        */}
       </body>
     </html>
-  );
-}
-
-export default function App() {
-  return <Outlet />;
+  )
 }
---
+++ app/routes/jokes._index.tsx
@@ -0,0 +1,13 @@
+export default function JokesIndexRoute() {
+    return (
+        <div>
+            <p>Here's a random joke:</p>
+            <p>
+                I was wondering why the frisbee was getting bigger,
+                then it hit me.
+            </p>
+        </div>
+    );
+}
+
+// Remember, URL is made from the file name!
